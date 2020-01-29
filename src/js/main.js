e03daaf34059955447d6181ef555d98c4c290a80
Roman Govin <rgovin@allvision.by>
2020-01-30 00:47:20 +0300

    30.01.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-import '../scss/styles.sass';
+// const slider = require('./slick/slick.min.js');
+// import './jquery.min.js';
+// const $ = require('jquery');
+import $ from './jquery.min.js';
+import './slick/slick.min.js';
+
+$('.myslider').slick({
+    infinite      : true,
+    slidesToShow  : 3,
+    slidesToScroll: 3
+});
